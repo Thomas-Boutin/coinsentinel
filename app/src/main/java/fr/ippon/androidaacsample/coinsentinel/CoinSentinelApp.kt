c0d2cbe633baa59d2070459239755315a8f70cdd
tboutin <tboutin@ippon.fr>
2018-07-23 15:32:47 +0200

    2-Timber_Stetho_StrictMode

---
+++ app/src/main/java/fr/ippon/androidaacsample/coinsentinel/CoinSentinelApp.kt
@@ -0,0 +1,34 @@
+package fr.ippon.androidaacsample.coinsentinel
+
+import android.app.Application
+import android.os.StrictMode
+import com.facebook.stetho.Stetho
+import timber.log.Timber
+
+class CoinSentinelApp : Application() {
+
+    override fun onCreate() {
+        if (BuildConfig.DEBUG) {
+            initializeDebugConfiguration()
+        }
+
+        super.onCreate()
+    }
+
+    private fun initializeDebugConfiguration() {
+        Stetho.initializeWithDefaults(this)
+        Timber.plant(Timber.DebugTree())
+        StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectAll()
+                .penaltyLog()
+                .build())
+        StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .penaltyDeath()
+                .build())
+    }
+}
